e46946b4ddfb3e07759df2489993b009eea7f1df
Riches-46 <107435193+Riches-46@users.noreply.github.com>
2022-06-14 21:50:48 +0100

    Create 4 functions and pass 2 arguments

--- hello.py
+++ hello.py
@@ -1,1 +1,26 @@
-print("Hello Rahul")
+def addition():
+    print(6 + 7)
+
+addition()
+
+def subtraction():
+    print(10 - 2)
+
+subtraction()
+
+def multiplication():
+    print(6 * 7)
+
+multiplication()
+
+def division():
+    print(42 / 7)
+
+division()
+
+
+  
+def arg(mess,app):
+    print(mess + app)
+
+arg("i love", "to program")
