0242427444fdf229bdec955d15a486ac62bcb29a
Riches-46 <107435193+Riches-46@users.noreply.github.com>
2022-06-20 16:38:59 +0100

    homework

--- hello.py
+++ hello.py
@@ -1,23 +1,23 @@
 #adds two numbers from  user input
-var1 = int(input("enter number"))
-var2 = int(input("enter another number"))
+var1 = int(input("enter number "))
+var2 = int(input("enter another number "))
 sum = (var1) + (var2)
-print(f"the total is {sum}")
+print(f"the total of two inputed numbers being added is {sum}")
 
-var1 = int(input("enter number"))
-var2 = int(input("enter another number"))
+var1 = int(input("enter number "))
+var2 = int(input("enter another number "))
 sum = (var1) - (var2)
-print(f"the total is {sum}")
+print(f"the total of two inputed numbers being subtracted is {sum}")
 
-var1 = int(input("enter number"))
-var2 = int(input("enter another number"))
+var1 = int(input("enter number "))
+var2 = int(input("enter another number "))
 sum = (var1) * (var2)
-print(f"the total is {sum}")
+print(f"the total of two inputed numbers being multiplied is {sum}")
 
-var1 = int(input("enter number"))
-var2 = int(input("enter another number"))
+var1 = int(input("enter number "))
+var2 = int(input("enter another number "))
 sum = (var1) / (var2)
-print(f"the total is {sum}")
+print(f"the total of two inputed numbers being divided is {sum}")
 
 
   
