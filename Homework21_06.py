1c3299de86101559c77faf763cc97d5d560f3c68
Riches-46 <107435193+Riches-46@users.noreply.github.com>
2022-06-21 19:56:05 +0100

    samples of todays lessons

--- Homework21_06.py
+++ Homework21_06.py
@@ -1,1 +1,28 @@
-names = ()
+#create a list
+names = ['richie','summer','rachel','richard']
+ages = ['9','11','46','47']
+
+#adds a name to the end of list
+names.append('bob')
+#prints name in spot 3
+print(names[3])
+#finds names in the first two positions
+print(names[:2])
+#finds a name in list
+print('richie'in names)
+#concantinates the two list
+print(names + ages)
+#extends one list to another
+ages.extend(names)
+#turns a lis into a tuple
+ages = tuple(ages)
+
+#creates a dictionary
+richdict = {'name': 'nick','age': '16','address': '6 turnout lane',
+            'phone numbe': '0851111111'}
+#prints a value from the dictionary
+print(richdict['age'])
+#prints key value names
+print(richdict.keys())
+#
+print(f "the address of nick is "{richdict[address]})
