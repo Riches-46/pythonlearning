517e7d00e56f883a5a89bbe60b99210f9e67b640
Riches-46 <107435193+Riches-46@users.noreply.github.com>
2022-06-21 19:04:29 +0100

    no change

--- Homework20_06.py
+++ Homework20_06.py
@@ -1,16 +1,16 @@
 #defining a function that subtracts
 def subtract(var1:int,var2:int):
-    var3 = var1 - var2
+    var3 = var1 + var2
     return (var3)
 #defining a function that divides
 def division(var1,var2):
-    var3 = var1 / var2
+    var3 = var1 + var2
     return (var3)
 
 #main
 def main():
-    a = float(input("enter number "))
-    b = float(input("enter another number "))
+    a = (input("enter number "))
+    b = (input("enter another number "))
     c = subtract(a,b)
     print(f"value of c is {c}")
     d = division(a,b)
