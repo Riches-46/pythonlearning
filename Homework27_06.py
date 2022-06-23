27c923966b4de75f79a842452d12d898be7f721d
Riches-46 <107435193+Riches-46@users.noreply.github.com>
2022-06-23 22:05:24 +0100

    need a function

--- Homework27_06.py
+++ Homework27_06.py
@@ -1,22 +1,18 @@
+#def myfunc():
 
-# creating an empty list
-from re import A
-from tkinter import N
-
-#empty list
-list1 = []
+my_list1 = []
  
-# number of elements as input
+    # number of elements as input
 n = int(input("Enter number of elements : "))
 
-#asserts to make sure there are only 5 elements
+    #asserts to make sure there are only 5 elements
 assert((n) < 6 and (n) > 4)  
 
-# iterating till the list is populated
+    # iterating till the list is populated
 for i in range(0, n):
     ele = int(input())
     
     # adding the element being input to the list
-    list1.append(ele) 
+    my_list1.append(ele) 
  
-print(list1)
+print(my_list1)
