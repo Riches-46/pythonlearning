45c07fe48431cea7d66d0bf990b02cc46b09b7c8
Riches-46 <107435193+Riches-46@users.noreply.github.com>
2022-06-26 10:47:44 +0100

    complete

--- Homework27_06.py
+++ Homework27_06.py
@@ -1,18 +1,23 @@
-#def myfunc():
-
-my_list1 = []
- 
+#define the function
+def new():
+    #create an empty list
+    my_list1 = []
     # number of elements as input
-n = int(input("Enter number of elements : "))
+    n = int(input("Enter amount of numbers in list : "))
 
     #asserts to make sure there are only 5 elements
-assert((n) < 6 and (n) > 4)  
+    assert((n) < 6 and (n) > 4)  
 
     # iterating till the list is populated
-for i in range(0, n):
-    ele = int(input())
+    for i in range(0, n):
+        num1 = int(input())
     
     # adding the element being input to the list
-    my_list1.append(ele) 
- 
-print(my_list1)
+        my_list1.append(num1) 
+    print(my_list1)
+    
+def main():
+    new()
+if __name__ == "__main__":
+    main()
+    
