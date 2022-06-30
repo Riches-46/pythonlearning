de4e6c9a76735724d781a1ffffad1325ec4b51a3
Riches-46 <107435193+Riches-46@users.noreply.github.com>
2022-06-30 18:23:09 +0100

    done

--- updated homework.py
+++ updated homework.py
@@ -30,20 +30,10 @@ def new():
             # adding the element being input to the list
             my_list1.append(division(num1,num2))
         elif operate == "X" or operate == "x":
-            break
-    for i in range(0, n):
-        operate = input("Enter A to operate or X to close")
-        if operate == "A" or operate == "a":
-            num1 = int(input("Enter the first number"))
-            num2 = int(input("Enter the second number"))
-            assert(num2 > 0)
-            # adding the element being input to the list
-            my_list1.append(addition(num1,num2))
-        elif operate == "X" or operate == "x":
-            break    
+            break   
     print(my_list1)
     
-def main():
+def main()
     new()
 if __name__ == "__main__":
     main()
