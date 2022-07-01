cbb7e5a7aa45e0ae3df3a14b2161c19377e0d3f6
Riches-46 <107435193+Riches-46@users.noreply.github.com>
2022-07-01 20:14:57 +0100

    almost

---
+++ new 30.06Homework.py
@@ -0,0 +1,63 @@
+
+def addition(var1:int,var2:int):
+    var3 = var1+var2
+    return var3
+def subtraction(var1:int,var2:int):
+    var4 = var1-var2
+    return var4
+
+n = 1
+f = open("new_table.csv","w")
+   
+#num1 = input ("Enter first number: ")
+#num2 = input("Enter Second number: ")
+#operator = input("Please enter either addition or subtraction: ")
+
+for i in range(0, n):
+        operate = input("Enter A to operate or X to close")
+        if operate == "A" or operate == "a":
+            num1 = int(input("Enter the first number : "))
+            num2 = int(input("Enter the second number : "))
+            operator = str(input("Enter either addition or subtraction : "))
+            assert(num2 > 0)   
+        elif operate == "X" or operate == "x":
+            break
+        results = operator 
+        if operator == addition:
+            print(addition)
+        elif operator == subtraction:
+            print(subtraction) 
+            b   
+file = open ("new_table.csv", "a")
+file.write ("Number 1 Number 2 operation Result\n")
+file.write ("{}  {}  {}   {} \n".format(num1, num2, operator, results))
+
+#def new():
+    #create an empty list
+    #my_list1 = []
+    #n = 1
+    # iterating till the list is populated
+    #for i in range(0, n):
+        #operate = input("Enter A to operate or X to close")
+        #if operate == "A" or operate == "a":
+            #num1 = int(input("Enter the first number"))
+            #num2 = int(input("Enter the second number"))
+            #assert(num2 > 0)
+            # adding the element being input to the list
+            #my_list1.append(addition(num1,num2))
+        #elif operate == "X" or operate == "x":
+            #break   
+    #print(my_list1)
+    
+#def main():
+    #new()
+#if __name__ == "__main__":
+    #main()
+
+
+
+
+
+
+
+file.close()
