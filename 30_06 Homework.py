387a6b02c81d92bf7b0e9b96f06a701f17defa22
Riches-46 <107435193+Riches-46@users.noreply.github.com>
2022-07-01 15:42:59 +0100

    trying to write to csv file

--- 30_06 Homework.py
+++ 30_06 Homework.py
@@ -7,7 +7,9 @@ import csv
 
 with open('myTable.csv','w+') as f:
     table = csv.writer(f)
+    #write a csv header
     table.writerow(["num1","num2","operation","results"])
+    #gets input from user
     numb1 = input("Please enter first number :")
     numb2 = input("Please enter second number :")
     operation1 = input("Please enter a math function :")
