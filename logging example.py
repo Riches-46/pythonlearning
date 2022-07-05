fcad1210ab9906bb24537ece56335fea9858cd45
Riches-46 <107435193+Riches-46@users.noreply.github.com>
2022-07-05 19:54:31 +0100

    no change

--- logging example.py
+++ logging example.py
@@ -1,7 +1,7 @@
 import logging
 from pathlib import Path
 import datetime
-#def log_inform():
+
 dir_data = Path("log")
 dir_data.mkdir(parents=True, exist_ok=True)
 log_filename = datetime.datetime.now().strftime("%Y.%m.%d-%H.%M.%S")
@@ -18,11 +18,9 @@ formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
 console.setFormatter(formatter)
 logging.getLogger().addHandler(console)
 logger1 = logging.getLogger("example")
-#a = 15 
-#b = 20
-#logger1.info(f"the value of a is {a} and the value of b is {b}")
-#res = a + b
-#logger1.info(f"the result is {res}")
-if __name__ == "__log_inform__":
-    log_inform()
+a = 15 
+b = 20
+logger1.info(f"the value of a is {a} and the value of b is {b}")
+res = a + b
+logger1.info(f"the result is {res}")
 
