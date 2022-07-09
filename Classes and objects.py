300c0934ee94dc0ae65bacb0e6cac4583e8c6186
Riches-46 <107435193+Riches-46@users.noreply.github.com>
2022-07-09 10:26:00 +0100

    classes

--- Classes and objects.py
+++ Classes and objects.py
@@ -1,10 +1,12 @@
+#Create a class
 class newClass:
     x = 50 
     
 print(newClass.x)
 
 class newClass1:
-    x=100
+    x = 100
+#create a new object    
 r = newClass1()
 print(r.x)
 
@@ -16,3 +18,20 @@ class student:
 
 s = student()
 s.display()
+
+#init function
+class student:
+    def __init__(self, name, dob):
+        self.name = name
+        self.dob = dob
+s1 = student('harry', '24/06/1975')    
+print(s1.name)
+print(s1.dob)
+
+#modify objects
+s1.name = 'richard'
+print(s1.name)
+
+#delete objects
+del s1.name
+print(s1.name)
