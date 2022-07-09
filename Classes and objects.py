c39404c52ecffd76b1e67963b78809a54e8e0ea0
Riches-46 <107435193+Riches-46@users.noreply.github.com>
2022-07-09 10:05:37 +0100

    Classes

---
+++ Classes and objects.py
@@ -0,0 +1,18 @@
+class newClass:
+    x = 50 
+    
+print(newClass.x)
+
+class newClass1:
+    x=100
+r = newClass1()
+print(r.x)
+
+class student:
+    id = 10
+    name = 'anderson'
+    def display(self):
+        print(self.id,self.name)
+
+s = student()
+s.display()
